71d2692818220ec43ade44472e33fe1fe131b4c6
Steve Mask <smask@paypal.com>
2020-07-08 16:12:38 -0400

    fix

--- src/PayPalButtons.js
+++ src/PayPalButtons.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 
 export default function PayPalButtons (props) {
   let Button = window.paypal.Buttons.driver('react', { React, ReactDOM });
-  Object.keys(props).forEach(prop => {
-    window.paypal.Buttons[prop] = props[prop];
-  });
 
   return (
     <Button />
